3ef3d823efec19edc9748587a89134017d446589
Youssef Raad <134856706+YoussefRaad-mathecon@users.noreply.github.com>
2024-10-17 16:52:34 +0200

    Update Data.R

--- Data.R
+++ Data.R
@@ -26,6 +26,7 @@ library(quadprog)
 
 
 set.seed(123)
+setwd("C:/Users/youss/OneDrive - University of Copenhagen/PUK")
 ####################################################################################################################
 ####################################################################################################################
 #-------------------------- Kenneth R French Data Library Data -----------------------------------------------------
@@ -40,7 +41,7 @@ impute_data_monthly <- function(data) {
   
   # Check for missing values and impute if necessary
   if (anyNA(data)) {
-    imputed_data <- mice(data, m = 5, method = 'pmm', maxit = 50, seed = 123)
+    imputed_data <- mice(data, m = 1, method = 'pmm', maxit = 50, seed = 123)
     data <- complete(imputed_data)
   }
   
@@ -51,17 +52,13 @@ impute_data_monthly <- function(data) {
 
 
 impute_data_annual <- function(data) {
-  
   # Convert all other columns to numeric and reassemble into a dataframe
   data <- as.data.frame(lapply(data, function(x) as.numeric(as.character(x))))
-  
   # Check for missing values and impute if necessary
   if (anyNA(data)) {
     imputed_data <- mice(data, m = 1, method = 'pmm', maxit = 50, seed = 123)
     data <- complete(imputed_data)
   }
-  
-  
   data[[1]] <- as.numeric(data[[1]])
   return(data)
 }
@@ -192,7 +189,6 @@ MOMdep_Value_Weighted_Average_of_Prior_Returns <- impute_data_monthly(MOMdep_Val
 ####################################################################################################################
 ####################################################################################################################
 
-
 TSYdata <- read.csv("yield-curve-rates-1990-2023.csv", header = TRUE)
 sum(is.na(TSYdata)) # 20258 NA's
 colSums(is.na(TSYdata))
@@ -203,7 +199,7 @@ colSums(is.na(TSYdata))
 TSYdata$Date <- as.Date(TSYdata$Date, format = "%m/%d/%y")
 
 # OBS: for some reason x2.Mo won't remove NA's in X2.M0; suspect it is because of high missingness with high colinearity
-TSYdata_imputed <- mice(TSYdata, m = 5, method = 'pmm', maxit = 50, seed = 123)
+TSYdata_imputed <- mice(TSYdata, m = 1, method = 'pmm', maxit = 50, seed = 123)
 
 # Extract completed data
 TSYdata <- complete(TSYdata_imputed)
@@ -218,69 +214,17 @@ sum(is.na(TSYdata)) # 20258 NA's
 #---------------------------------- Monthly Returns ----------------------------------------------------------------
 ####################################################################################################################
 ####################################################################################################################
+#see python file for the function to output the CSV that we load underneath.
+Bonds <- read.csv("bonds_with_returns.csv", header = TRUE, sep = ",", fill = TRUE, strip.white = TRUE)
+Bonds <- Bonds[, c("Date", "Monthly.Return.10.Yr")]
+Bonds$Date_numeric <- as.numeric(as.Date(Bonds$Date))
 
-# Convert yields from percentages to decimal values
-TSYdata_dec <- TSYdata %>%
-  mutate(across(-Date, ~ .x / 100)) # This converts all columns except Date to decimals
+# Impute
+Bonds_imputed <- mice(Bonds, method = 'pmm', m = 5, maxit = 50, seed = 123) # Exclude the original Date for imputation
+Bonds <- complete(Bonds_imputed)
+Bonds <- data.frame(Date = Bonds$Date, Monthly.Return.10.Yr = Bonds$Monthly.Return.10.Yr)
 
-# Resample to monthly data using the last value in each month
-monthly_TS <- TSYdata %>%
-  mutate(YnM = floor_date(Date, "month")) %>%
-  group_by(YnM) %>%
-  slice_max(Date, with_ties = FALSE) %>%  # Get the last row for each month
-  ungroup() %>%
-  arrange(YnM)
+Bonds$Date <- as.Date(Bonds$Date)
+write.csv(Bonds, file = "Bonds_done.csv", row.names = FALSE)
+Bonds <- Bonds$Monthly.Return.10.Yr
 
-
-# Define the PV and get_r functions
-PV <- function(C, Y, T) {
-  pv <- 0
-  if (T == 10) {
-    t_j <- seq(1, 9)
-  } else {
-    t_j <- seq(1, 9) - 1/12
-  }
-  for (i in t_j) {
-    pv <- pv + C / (1 + Y)^i
-  }
-  pv <- pv + C / (1 + Y)^T + 1 / (1 + Y)^T
-  return(pv)
-}
-
-get_r <- function(Yim1, Yi) {
-  Ti <- 10  # Fixed value for the 10-year maturity
-  Tim1 <- Ti - 1/12  # Previous time step (1 month earlier)
-  
-  pv1 <- PV(Yim1, Yi, Tim1)  # Present value with shifted yield and current yield
-  pv2 <- PV(Yim1, Yim1, Ti)  # Present value with shifted yield twice
-  
-  # Return the relative change in present values
-  r <- pv1 / pv2 - 1
-  return(r)
-}
-
-# Compute the monthly returns for the 10-Year yield
-monthly_TS <- monthly_TS %>%
-  mutate(X10.Yr.1M = lag(X10.Yr),
-         X10.Yr.Return = mapply(get_r, X10.Yr.1M, X10.Yr))
-
-monthly_TS <- monthly_TS %>%
-  mutate(`10 Yr m1` = lag(`X10.Yr`),  # Shift the 10-Year yield by one month
-         `10YrReturns` = mapply(get_r, `10 Yr m1`, `X10.Yr`))  # Apply the function element-wise
-
-Bonds <- monthly_TS$X10.Yr.Return
-
-
-# Filter the data 1990-01-02 to 1990-01-31
-TSYdata_1990_Jan <- TSYdata %>%
-  filter(Date >= as.Date("1990-01-02") & Date <= as.Date("1990-01-31"))
-
-# Calculate the mean for each bond
-avg_1990_Jan <- colMeans(TSYdata_1990_Jan[, c("X1.Mo", "X2.Mo", "X3.Mo", "X4.Mo", "X6.Mo", 
-                                              "X1.Yr", "X2.Yr", "X3.Yr", "X5.Yr", "X7.Yr", 
-                                              "X10.Yr", "X20.Yr", "X30.Yr")])
-avg_1990_Jan <- avg_1990_Jan/100
-
-
-Bonds[1] <- avg_1990_Jan[11]
-RF <- FFdata_Monthly_Factors$RF ### RF rate
